145b1d0757f2ab4180f07cc3fc667c1f993c7d77
Marco Eilers <eilers.marco@googlemail.com>
2013-07-10 18:42:30 +0200

    Added optional debug output

--- typeEffect/Main.hs
+++ typeEffect/Main.hs
@@ -17,6 +17,7 @@ main = do
   args <- getArgs
   let fname = if (length args) > 0 then args !! 0 else error "Please provide a file name"
       level = if (length args) > 1 then read (args !! 1) else 2
+      debug = if (length args) > 2 then read (args !! 2) else 0
   input <- readFile fname
   let res = parseString input
   case res of
@@ -27,14 +28,16 @@ main = do
           res = solveConstr ty constr
           resTy = applyConstr ty res
       putStrLn "Success"
-      printAll $ M.toList map
+      printAll debug $ M.toList map
 
-printAll :: [(Int, (LTerm, TEnv, SType, TSubst, Constraint))] -> IO ()
-printAll [] = return ()
-printAll ((l, (tm, tenv, ty, sub, constr)):rest) = do
+printAll :: Int -> [(Int, (LTerm, TEnv, SType, TSubst, Constraint))] -> IO ()
+printAll _ [] = return ()
+printAll db ((l, (tm, tenv, ty, sub, constr)):rest) = do
   let constrSolvd = solveConstr ty constr
       resTy = applyConstr ty constrSolvd
-  putStrLn $ "Label " ++ (show l) ++ " is term " ++ (show tm) ++ ",\nhas type " ++ (show resTy)
-  printAll rest
+  putStrLn $ if db > 0 
+             then "Label " ++ (show l) ++ " is term " ++ (show tm) ++ ",\nhas preliminary type "++ (show ty) ++ " and constraints " ++ (show constr) ++ ",\nfinal type is " ++ (show resTy)
+             else "Label " ++ (show l) ++ " is term " ++ (show tm) ++ ",\nhas type " ++ (show resTy)
+  printAll db rest
 
 
